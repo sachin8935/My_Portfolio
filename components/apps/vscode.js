ea83e7877c003170807a1078abbb70ffd1f524f5
sachin8935 <sachin89359@gmail.com>
2024-05-16 15:19:42 +0530

    updated

---
+++ components/apps/vscode.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function VsCode() {
+  return (
+    <iframe
+      src="https://github1s.com/sachin8935/EduConnect"
+      title="VsCode"
+      className="h-full w-full bg-ub-cool-grey"
+    ></iframe>
+  );
+}
+
+export const displayVsCode = () => {
+  <VsCode> </VsCode>;
+};
